8a2e47e61131c7b6aeee90f4008d1ae62e1a9b1d
Victoria <onevermore233@gmail.com>
2020-11-24 17:11:23 +0400

    added ? to data.pokemons.map

--- src/components/PokedexList/index.tsx
+++ src/components/PokedexList/index.tsx
@@ -3,14 +3,14 @@ import { PokemonCard, PokemonsProps } from '../PokemonCard';
 import s from './PokedexList.module.scss';
 
 interface DataPokemon {
-  pokemons: PokemonsProps[];
+  pokemons?: PokemonsProps[];
 }
 
 export const PokedexList: React.FC<DataPokemon> = ({ pokemons }) => {
   return (
     <>
       <div className={s.root}>
-        {pokemons.map((pokemon) => {
+        {pokemons?.map((pokemon) => {
           return <PokemonCard data={pokemon} key={pokemon.id} />;
         })}
       </div>
