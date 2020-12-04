af29afa4fcf409922770c92535d3f40870fc64a0
Victoria <onevermore233@gmail.com>
2020-12-04 15:16:59 +0400

    fixed svg output path

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -47,7 +47,7 @@ module.exports = {
         use: ['@svgr/webpack', 'url-loader'],
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: 'file-loader',
